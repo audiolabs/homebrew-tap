12241602c4aa379311745b4863e774c8640948b9
Fabian-Robert Stöter <fabian-robert.stoeter@audiolabs-erlangen.de>
2014-05-15 13:59:15 +0200

    VMPK Cask

---
+++ Casks/vmpk.rb
@@ -0,0 +1,6 @@
+class Vmpk < Cask
+  homepage 'http://sourceforge.net/projects/vmpk/files/'
+  url 'http://downloads.sourceforge.net/project/vmpk/vmpk/0.5.1/vmpk-0.5.1-mac32-universal.dmg'
+  sha256 '66e91c671179b98b0264c386eb0d0d667ebd84486e781dba59b52fc2662c0f16'
+  version '0.5.1'
+end
