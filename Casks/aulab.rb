1615b2e8448bcae5a233e4da1ed9bdaa3a3debbc
Nils Werner <nils.werner@audiolabs-erlangen.de>
2014-05-23 12:56:16 +0200

    Audiolab Cask

---
+++ Casks/aulab.rb
@@ -0,0 +1,7 @@
+class Aulab < Cask
+  url 'https://www.apple.com/euro/itunes/mastered-for-itunes/docs/au_lab.zip'
+  homepage 'https://www.apple.com/de/itunes/mastered-for-itunes/'
+  version '2.2.2'
+  sha256 'a4642cd86474597f64f84a0b7ff274ad9ebf7f5169fa7c4f562620733555369f'
+  nested_container 'AU Lab Image.dmg'
+end
