b0b3d09901d3eba18dee7ec9f26da299c1cdd01a
Nils Werner <nils.werner@audiolabs-erlangen.de>
2014-05-23 12:54:46 +0200

    Spot Color Cask

---
+++ Casks/spotcolor.rb
@@ -0,0 +1,6 @@
+class Spotcolor < Cask
+  url 'https://github.com/nils-werner/SpotColor/releases/download/1.0.3/Spot.Color.dmg'
+  homepage 'https://github.com/nils-werner/SpotColor'
+  version '1.0.3'
+  sha256 'f4b744b98dd109b018783f797bd78292d158e994f5d4c53a451807d1ce55927e'
+end
