4962652ed5be92596d26e3c5cfe1d65a7097f851
Nils Werner <nils.werner@audiolabs-erlangen.de>
2014-07-18 12:54:57 +0200

    Pyo formula

---
+++ Casks/pyo.rb
@@ -0,0 +1,6 @@
+class Pyo < Cask
+  url 'http://ajaxsoundstudio.com/downloads/pyo_0.6.9_OSX-universal.dmg'
+  homepage 'http://ajaxsoundstudio.com/software/pyo/'
+  version '0.6.9'
+  sha256 'd52d9c797b70c8f3d95a38d439882c226af76115af3c86ef4fdeda4d3231a079'
+end
