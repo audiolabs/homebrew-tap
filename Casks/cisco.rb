b6dc5708e00c742b3b0b250fafaf8e1674525833
Nils Werner <nils.werner@audiolabs-erlangen.de>
2014-12-10 09:55:02 +0100

    Fixed Cisco hash

--- Casks/cisco.rb
+++ Casks/cisco.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'cisco' do
   version '3.1.05152'
-  sha256 '7136864e0b27d8b2c153abbebb7f0c09b96e7d3a93824eb307e6044332b1fbfb'
+  sha256 '5710c674e238244d294cddb55b57d54ea28050764d914bb54f91c96ccf95c325'
 
   url 'http://www.rrze.uni-erlangen.de/dienste/internet-zugang/vpn/anyconnect-macosx-i386-3.1.05152-k9.dmg'
   homepage 'hhttp://www.rrze.uni-erlangen.de/dienste/internet-zugang/vpn/cisco-vpn.shtml'
